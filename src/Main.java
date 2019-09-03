3625aabf9ab52793dd767e516a430220052c769b
micah-clarke <48541196+micah-clarke@users.noreply.github.com>
2019-09-03 16:58:24 +1000

    Main class updated with the car going onto road 2. The car waits for the traffic light to turn green, depending on the direction it is travelling, then turns left or right depending on a random function. The output message shows the car waiting at the traffic light, the direction the car turned, and which road the car turned onto

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
         Road road = new Road(2, 10, 5, 0, "01");
         Car car = new Car(2, 1, road.getStartXPosition(), road.getStartYPosition(), 1);
+        Road road1 = new Road(10, 2, 0, 11, "02");
+        TrafficLight trafficLight = new TrafficLight(true, false);
+        int waitTime = 0;
 
         while (car.getxPosition() <= road.getXFinish() && car.getyPosition() <= road.getYFinish()) {
             if (road.getLength() == 2) {
@@ -19,6 +22,42 @@ public class Main {
             }
         }
 
+        if (car.getyPosition() == road1.getStartYPosition()) {
+            double direction = ( + Math.random( ) );
+                if (road1.getHeight() == 2) {
+                    while (trafficLight.isXGreen()) {
+                        car.stopX();
+                        waitTime += 1;
+                        System.out.println("Car is waiting at a traffic light on road " + road.getRoadID());
+                        if (waitTime == 10){
+                            trafficLight.changeX();
+                        }
+
+                    }
+                        if (direction > 0.5) {
+                            car.setSpeed(-car.getSpeed());
+                            System.out.println("Car turned left onto road " + road1.getRoadID());
+                            while (car.getxPosition() <= road1.getXFinish() && car.getxPosition() >= road1.getStartXPosition()) {
+                                car.driveX();
+                                System.out.println("Car is at x: " + car.getxPosition() + " y: " + car.getyPosition() + " on road: " + road1.getRoadID());
+                            }
+
+                        } else {
+                            System.out.println("Car turned right onto road " + road1.getRoadID());
+                            while (car.getxPosition() <= road1.getXFinish()) {
+                                car.driveX();
+                                System.out.println("Car is at x: " + car.getxPosition() + " y: " + car.getyPosition() + " on road: " + road1.getRoadID());
+                            }
+
+
+                        }
+                }
+                else{
+                    System.out.println("Car continued onto road " + road1.getRoadID());
+                    while (car.getyPosition() <= road1.getYFinish() && car.getyPosition() >= road1.getStartYPosition())
+                    car.driveY();
+                }
+        }
 
     }
 }
