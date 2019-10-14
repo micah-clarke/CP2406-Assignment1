fe7679a20fbe9c2758a682cac21d39348c709864
micah-clarke <48541196+micah-clarke@users.noreply.github.com>
2019-10-14 23:46:02 +1000

    Add an openable JFrame for when edit roads is pressed Add mouseListener to the JFrame to find when the start x and y, and end x and y coordinates are for the new road

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -2,12 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.Random;
 
 
-public class Assignment2 extends JFrame implements ActionListener, MouseListener {
+public class Assignment2 extends JFrame implements ActionListener{
     private JButton newRoad = new JButton("Add Road");
     private JButton editMode = new JButton("Edit mode");
     private  JButton runMode = new JButton("Run mode");
@@ -20,15 +19,72 @@ public class Assignment2 extends JFrame implements ActionListener, MouseListener
     private int startRoadY;
     private int endRoadX;
     private int endRoadY;
+    private RoadEditorPanel roadEditorPanel = new RoadEditorPanel();
 
-    public class RoadEditor extends JPanel {
-        RoadEditor(){
-            this.setPreferredSize(new Dimension(500, 500));
-            this.add(new JLabel("Hello test whatever"));
+    public class RoadEditorPanel extends JFrame {
+        RoadEditorPanel(){
+            super("Road Editor");
+            setSize(600, 600);
+            JPanel roadEditorPanel = new JPanel();
+            roadEditorPanel.setLayout(new BorderLayout());
+            roadEditorPanel.setSize(500, 500);
+            roadEditorPanel.setBackground(Color.WHITE);
+            JButton saveEditedRoads = new JButton("Save Roads");
+            add(saveEditedRoads, "North");
+            saveEditedRoads.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    setVisible(false);
+                    clickNumber = 1;
+                }
+            });
+            roadEditorPanel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
+                    if(isVisible()){
+                        {
+                            if(clickNumber == 1){
+                                startRoadX = e.getX();
+                                startRoadY = e.getY();
+                                System.out.println("Click one is at " + startRoadX + ", " + startRoadY);
+                            }
+                            if(clickNumber == 2){
+                                endRoadX = e.getX();
+                                endRoadY = e.getY();
+
+                                System.out.println("Click two is at " + endRoadX + ", " + endRoadY);
+                            }
+                            if(clickNumber >= 2){
+                                clickNumber = 0;
+                            }
+                            clickNumber +=1;
+
+
+
+
+                        }
+                    }
+                }
+            });
+            add(roadEditorPanel);
+
+            boolean isVertical = Math.abs(startRoadX - endRoadX) > Math.abs(startRoadY = endRoadY);
+
+            if(isVertical){
+                Road road = new Road(startRoadX, startRoadY, Math.abs(startRoadX - endRoadX), 3, "1");
+            }
+            else{
+                Road road = new Road(startRoadX, startRoadY,3 ,  Math.abs(startRoadY - endRoadY), "1");
+            }
+
+
         }
+
+
     }
 
-    public Assignment2(){
+    private Assignment2(){
         setTitle("Assignment 2");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
@@ -51,31 +107,32 @@ public class Assignment2 extends JFrame implements ActionListener, MouseListener
 
 
     }
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
         output.setSize(500, 500);
-
-
+        output.add(new JLabel("Please work"));
 
         if(source == editMode){
             runMode.setVisible(false);
             newRoad.setVisible(true);
             saveRoads.setVisible(true);
-            if(source == newRoad){
-                RoadEditor roadEditor = new RoadEditor();
-                roadEditor.setVisible(true);
+            System.out.println("Pressed edit mode");
 
+        }
+        else if(source == newRoad){
+            roadEditorPanel.setVisible(true);
+            output.add(new JLabel("Bazinga"));
+            System.out.println("Pressed new road");
 
-            }
         }
         else if(source == saveRoads){
             startRun.setVisible(true);
             runMode.setVisible(true);
             newRoad.setVisible(false);
             saveRoads.setVisible(false);
+            output.add(new JLabel("Kachow"));
+            System.out.println("Pressed save roads");
         }
 
 
@@ -85,40 +142,7 @@ public class Assignment2 extends JFrame implements ActionListener, MouseListener
         }
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-            if(clickNumber == 1){
-                startRoadX = e.getX();
-                startRoadY = e.getY();
-                clickNumber = 2;
-            }
-            if(clickNumber == 2){
-                endRoadX = e.getX();
-                endRoadY = e.getY();
-                clickNumber = 1;
-            }
 
-    }
-
-    @Override
-    public void mousePressed(MouseEvent mouseEvent) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent mouseEvent) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent mouseEvent) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent mouseEvent) {
-
-    }
 
 
     public static void main (String[] args){
